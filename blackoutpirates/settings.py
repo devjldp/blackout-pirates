58984ed6ef6e4fedfe236f20727233a094a411bd
JoseDev <132334488+devjldp@users.noreply.github.com>
2024-06-20 09:12:46 +0100

    Create static file css and link it

--- blackoutpirates/settings.py
+++ blackoutpirates/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l-twuiw7=eaxdw!wi^(qlo0$f7-2-m6-drv*g_=hcp$)&gz-=r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
