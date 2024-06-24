b978cbb11f3ca1ac80577b319fae13158bda6918
iungoJose <jose.diaz@iungo.solutions>
2024-06-24 15:45:10 +0100

    Render upcoming concerts in home page

--- home/views.py
+++ home/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 
+# import a model from other app
+from concerts.models import Concert
+
+
 # Create your views here.
 
 def index(request):
-  """
-    A view to return the index page
-  """
-  return render(request, 'home/index.html')
+    """
+      A view to return the index page
+    """
+    concerts = Concert.objects.all()
+
+    return render(request, 'home/index.html', {"concerts": concerts})
