ae6d9d8f2e3624de4258523dbeee0de5ee798da1
JoseDev <132334488+devjldp@users.noreply.github.com>
2024-06-24 11:27:16 +0100

    Added concerts

--- concerts/admin.py
+++ concerts/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
-from .models import Concerts
+from .models import Concert
 
 # Register your models here.
 
 
 
 # Register your models here.
-admin.site.register(Concerts)
+admin.site.register(Concert)
--- concerts/models.py
+++ concerts/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 # Create your models here.
 
-class Concerts(models.Model):
+class Concert(models.Model):
   city = models.CharField( max_length=60)
   date = models.DateField()
+  
+  def __str__(self):
+    return self.city
