b3559fda935f64a55b6b2bc920101fd31479a923
JoseDev <132334488+devjldp@users.noreply.github.com>
2024-06-24 11:41:56 +0100

    Customize concerts model

--- concerts/admin.py
+++ concerts/admin.py
@@ -4,7 +4,10 @@ from .models import Concert
 
 # Register your models here.
 
-
+class ConcertAdmin(admin.ModelAdmin):
+  list_display = ('city', 'date')
+  ordering = ['date'] # Ordereing Ascendent
+  # ordering = ['-date'] # Ordering descendent
 
 # Register your models here.
-admin.site.register(Concert)
+admin.site.register(Concert, ConcertAdmin)
