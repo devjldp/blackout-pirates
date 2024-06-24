1f7b35a15ce75c373d3d582d23dd39dcd67fc5c7
JoseDev <132334488+devjldp@users.noreply.github.com>
2024-06-24 12:03:00 +0100

    Render concerts info

--- concerts/views.py
+++ concerts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+from .models import Concert
+
+
 # Create your views here.
+
+def all_concerts(request):
+  """A view to show all concerts"""
+  
+  concerts = Concert.objects.all()
+  
+  context = {
+    'concerts': concerts
+  }
+  return render(request, 'concerts/concerts.html', context)
