2f1d0d8ee93020384079cf920b7f0d21acf8232b
iungoJose <jose.diaz@iungo.solutions>
2024-06-26 09:04:59 +0100

    Add base.html to templates

--- basket/contexts.py
+++ basket/contexts.py
@@ -3,26 +3,25 @@ from django.shortcuts import get_object_or_404
 from concerts.models import Concert
 
 
-
 def basket_contents(request):
     basket_tickets = []
     total = 0
     tickets_count = 0
 
-    basket = request.session.get('basket', {} )
+    basket = request.session.get('basket', {})
 
     for concert_id, quantity in basket.items():
-        concert = get_object_or_404(Concert, pk = concert_id)
-        total += quantity*concert.price # Sustituir por concert.price
+        concert = get_object_or_404(Concert, pk=concert_id)
+        total += quantity*concert.price  # Sustituir por concert.price
         subtotal = quantity * concert.price
         tickets_count += quantity
         basket_tickets.append({
             'concert_id': concert_id,
             'quantity': quantity,
             'city': concert.city,
-            'price':concert.price,
+            'price': concert.price,
             'subtotal': subtotal
-            })
+        })
     grand_total = total
     context = {
         "basket_tickets": basket_tickets,
