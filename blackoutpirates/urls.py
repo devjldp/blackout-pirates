46a1a12224d6536ac099746bb443b2ee53e72e83
iungoJose <jose.diaz@iungo.solutions>
2024-07-01 14:46:01 +0100

    Create urls for checkout

--- blackoutpirates/urls.py
+++ blackoutpirates/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('concerts/', include('concerts.urls')),
-    path('basket/', include('basket.urls'))
+    path('basket/', include('basket.urls')),
+    path('checkout/', include('checkout.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
