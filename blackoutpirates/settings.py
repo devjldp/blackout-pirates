327de2db91381d83991e101b88d229dca3cdf2c3
JoseDev <132334488+devjldp@users.noreply.github.com>
2024-07-01 21:13:13 +0100

    Set public and secret keys

--- blackoutpirates/settings.py
+++ blackoutpirates/settings.py
@@ -193,3 +193,9 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# stripe
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
+STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
--- checkout/views.py
+++ checkout/views.py
@@ -1,22 +1,46 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from basket.contexts import basket_contents
+
+import stripe
+
 # Create your views here.
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+    
+    
     basket = request.session.get('basket', {})
     if not basket:
         messages.error()
         return redirect(reverse('concerts'))
 
+    current_basket = basket_contents(request)
+    total = current_basket['grand_total']
+    stripe_total = round(total*100)
+    
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY
+    )
+    
     order_form = OrderForm()
+    
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing.\
+            Did you forget to set it in your environment?')
+    
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51PXpZKRur0OhULbvmPpuXKH0TfprzG2rlJxoa1fss3wbt8kYqi4WVPCYSOuoqgHbnJZIxfQIBpCjiGO7qj0I4jGA00p7wU63C6',
-        'client_secret': 'test client secret' 
+        'stripe_public_key': stripe_public_key,
+        'client_secret':intent.client_secret 
     }
 
     return render(request, template, context)
