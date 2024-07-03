5a5c37953a91694be3a0cd1dca41cdaa894e3116
JoseDev <jluisengineer@gmail.com>
2024-07-03 16:09:25 +0100

    Add user profile form and update profile view

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import UserProfile
 
+from .forms import UserProfileForm
 # Create your views here.
 
 
@@ -8,9 +9,13 @@ def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
     # print(profile)
+    form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
+
     template = 'profiles/profile.html'
     context = {
-        'profile': profile
+        'form': form,
+        'orders': orders
     }
-
+    print(form)
     return render(request, template, context)
