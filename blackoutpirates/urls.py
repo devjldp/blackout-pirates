8845ef0d4921f425a478478faadb1777616d2a88
JoseDev <jluisengineer@gmail.com>
2024-07-03 14:05:05 +0100

    Add profile html views and url

--- blackoutpirates/urls.py
+++ blackoutpirates/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path('', include('home.urls')),
     path('concerts/', include('concerts.urls')),
     path('basket/', include('basket.urls')),
-    path('checkout/', include('checkout.urls'))
+    path('checkout/', include('checkout.urls')),
+    path('profiles/', include('profiles.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
