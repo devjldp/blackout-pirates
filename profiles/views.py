eeed92f14082cff53e0f4f8d041a7044dec67192
JoseDev <132334488+devjldp@users.noreply.github.com>
2024-07-05 12:26:58 +0100

    Fix if else statement in profile view

--- profiles/views.py
+++ profiles/views.py
@@ -17,9 +17,12 @@ def profile(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile updated successfully')
+        else:
+            messages.error(request, 'Updated failed. Please ensure the form is vallid.')
             
     # print(profile)
-    form = UserProfileForm(instance=profile)
+    else:
+        form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
     template = 'profiles/profile.html'
