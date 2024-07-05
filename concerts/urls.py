ff3d6aa5d6343e3bc1fe514bd689030d0fd812d2
JoseDev <132334488+devjldp@users.noreply.github.com>
2024-07-05 12:40:14 +0100

    Create edit_concert template and url

--- concerts/urls.py
+++ concerts/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.all_concerts, name='concerts'),
     path('<int:concert_id>', views.concert_detail, name='concert_detail'),
-    path('add/', views.add_concert, name='add_concert')
+    path('add/', views.add_concert, name='add_concert'),
+    path('add/', views.edit_concert, name='edit_concert')
 ]
